637c85c7dad847a8f49c7e9a92f7f35010b82531
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-10-13 05:02:06 -0500

    Added new gobernor Alessa
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -524,6 +524,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALESSA)
+extern struct cpufreq_governor cpufreq_gov_alessa;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alessa)
 #endif
 
 /*********************************************************************
