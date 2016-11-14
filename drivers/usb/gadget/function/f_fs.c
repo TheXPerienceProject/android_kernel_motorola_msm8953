ce50ece16f9373879e057c94f84492d4a265da8a
Jerry Zhang <zhangjerry@google.com>
2016-11-14 15:33:57 -0800

    usb: gadget: f_fs: Add ioctl for allocating endpoint buffers.
    This creates an ioctl named FUNCTIONFS_ENDPOINT_ALLOC which will
    preallocate buffers for a given size. Any synchronous reads/writes
    on that endpoint below that size will use those buffers instead of
    allocating their own. If the endpoint is not active, the buffer
    will not be allocated until it becomes active.
    
    Signed-off-by: Jerry Zhang <zhangjerry@google.com>
    Change-Id: Iddc2eb9e33bd4c819fa4201b2a43ffdcd54ee415

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -138,6 +138,9 @@ struct ffs_epfile {
 
 	unsigned char			_pad;
 	atomic_t			opened;
+
+	unsigned long			buf_len;
+	char				*buffer;
 };
 
 /*  ffs_io_data structure ***************************************************/
@@ -792,10 +795,11 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 		spin_unlock_irq(&epfile->ffs->eps_lock);
 
 		if (!io_data->read)
-			data = kmalloc(data_len + extra_buf_alloc,
-					GFP_KERNEL);
-		else
-			data = kmalloc(data_len, GFP_KERNEL);
+			data_len += extra_buf_alloc;
+
+		data = (data_len > epfile->buf_len || io_data->aio) ?
+			kmalloc(data_len, GFP_KERNEL) :
+			epfile->buffer;
 		if (unlikely(!data))
 			return -ENOMEM;
 		if (io_data->aio && !io_data->read) {
@@ -963,7 +967,8 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 					}
 				}
 			}
-			kfree(data);
+			if (data_len > epfile->buf_len || io_data->aio)
+				kfree(data);
 		}
 	}
 
@@ -974,7 +979,8 @@ error_lock:
 	spin_unlock_irq(&epfile->ffs->eps_lock);
 	mutex_unlock(&epfile->mutex);
 error:
-	kfree(data);
+	if (data_len > epfile->buf_len || io_data->aio)
+		kfree(data);
 	if (ret < 0)
 		pr_err_ratelimited("Error: returning %zd value\n", ret);
 	return ret;
@@ -1145,7 +1151,7 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 		return -ENODEV;
 
 	spin_lock_irq(&epfile->ffs->eps_lock);
-	if (likely(epfile->ep)) {
+	if (epfile->ep) {
 		switch (code) {
 		case FUNCTIONFS_FIFO_STATUS:
 			ret = usb_ep_fifo_status(epfile->ep->ep);
@@ -1183,11 +1189,29 @@ static long ffs_epfile_ioctl(struct file *file, unsigned code,
 				ret = -EFAULT;
 			return ret;
 		}
+		case FUNCTIONFS_ENDPOINT_ALLOC:
+			kfree(epfile->buffer);
+			epfile->buffer = NULL;
+			epfile->buf_len = value;
+			if (epfile->buf_len) {
+				epfile->buffer = kzalloc(epfile->buf_len,
+						GFP_KERNEL);
+				if (!epfile->buffer)
+					ret = -ENOMEM;
+			}
+			break;
+
 		default:
 			ret = -ENOTTY;
 		}
 	} else {
-		ret = -ENODEV;
+		switch (code) {
+		case FUNCTIONFS_ENDPOINT_ALLOC:
+			epfile->buf_len = value;
+			break;
+		default:
+			ret = -ENODEV;
+		}
 	}
 	spin_unlock_irq(&epfile->ffs->eps_lock);
 
@@ -1755,6 +1779,8 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 			ep->ep->driver_data = NULL;
 		}
 		epfile->ep = NULL;
+		kfree(epfile->buffer);
+		epfile->buffer = NULL;
 
 		++ep;
 		++epfile;
@@ -1795,6 +1821,14 @@ static int ffs_func_eps_enable(struct ffs_function *func)
 
 		ep->ep->driver_data = ep;
 		ep->ep->desc = ds;
+		if (epfile->buf_len) {
+			epfile->buffer = kzalloc(epfile->buf_len,
+					GFP_KERNEL);
+			if (!epfile->buffer) {
+				ret = -ENOMEM;
+				break;
+			}
+		}
 
 		ret = config_ep_by_speed(func->gadget, &func->function, ep->ep);
 		if (ret) {
