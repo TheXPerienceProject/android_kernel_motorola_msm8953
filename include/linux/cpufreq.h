5aff787ffb1534d9c881bf7a627f3bc18115a9cb
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-11-19 05:43:13 -0600

    XPerience_governor: Initial port of my own governor XPerience Governor
    Change-Id: I788588bb1b17b1ada893194913efa45c12fc668d
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -530,6 +530,9 @@ extern struct cpufreq_governor cpufreq_gov_lisi;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALESSA)
 extern struct cpufreq_governor cpufreq_gov_alessa;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alessa)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
+extern struct cpufreq_governor cpufreq_gov_xperience;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
 #endif
 
 /*********************************************************************
