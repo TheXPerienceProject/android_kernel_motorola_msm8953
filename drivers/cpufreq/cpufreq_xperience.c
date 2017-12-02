31c177e06bce95585f104b96da0762d21b673041
klozz <klozz707@gmail.com>
2017-12-01 19:47:28 -0500

    xperience: tweak governor for addison using the freq located on msm8953.dtsi
    Change-Id: I75da38b8cd00748d7945d85c7e176c2e7af3a916
    Signed-off-by: klozz <klozz707@gmail.com>

--- drivers/cpufreq/cpufreq_xperience.c
+++ drivers/cpufreq/cpufreq_xperience.c
@@ -14,7 +14,7 @@
  *
  * Autor: Carlos Jesús B.C (Aka Klozz or TeamMEX@XDA)
  * Based on smartassv2
- * 
+ *
  */
 
 #include <linux/module.h>
@@ -38,7 +38,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ 787200
+#define DEFAULT_AWAKE_IDEAL_FREQ  1401600
 static unsigned int awake_ideal_freq;
 
 /*
@@ -47,7 +47,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ 600000 /*I Think 600mhz are good :D*/
+#define DEFAULT_SLEEP_IDEAL_FREQ 652800 /*I Think 652mhz are good :D*/
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -55,7 +55,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP 256000
+#define DEFAULT_RAMP_UP_STEP 356000
 static unsigned int ramp_up_step;
 
 /*
@@ -63,19 +63,19 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP 256000
+#define DEFAULT_RAMP_DOWN_STEP 356000
 static unsigned int ramp_down_step;
 
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 50
+#define DEFAULT_MAX_CPU_LOAD 65
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 22
+#define DEFAULT_MIN_CPU_LOAD 20
 static unsigned long min_cpu_load;
 
 /*
@@ -286,8 +286,8 @@ static void cpufreq_xperience_timer(unsigned long cpu)
 	delta_idle = cputime64_sub(now_idle, this_xperience->time_in_idle);
 	delta_time = cputime64_sub(update_time, this_xperience->idle_exit_time);
 
-	// If timer ran less than 1ms after short-term sample started, retry.
-	if (delta_time < 1000) {
+	// If timer ran less than 2ms after short-term sample started, retry.
+	if (delta_time < 2000) {
 		if (!timer_pending(&this_xperience->timer))
 			reset_timer(cpu,this_xperience);
 		return;
