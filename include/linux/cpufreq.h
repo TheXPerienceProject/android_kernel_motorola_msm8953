fc74c68fbfe2e61e5a4f0bda1c75cb93f604268c
klozz <klozz707@gmail.com>
2017-12-03 04:28:32 -0500

    cpufreq: add ZZMoove big.LITTLE Edition (bLE) all credits to ZaneZam
    working features in this first version:
    
    - zzmoove freq scaling method (without limit functionality)
    - proportional scaling (all modes)
    - fast scaling up/down with afs (fully functional)
    
    also squashed to the last version
    11.07.17
    Changelog (11.07.17):
    
    - moved 'prev_load' variable to per policy dbs structure
    - added missing evaluation of freq range when max/min frequency has changed
    - added more info to version variable: k3xx / k4xx = for kernel version 3.x.x or 4.x.x
      and last 6 digits are the date of develop version in format ddmmyy
    - documentation updated
    
    Signed-off-by: klozz <klozz707@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -518,6 +518,9 @@ extern struct cpufreq_governor cpufreq_gov_alessa;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XPERIENCE)
 extern struct cpufreq_governor cpufreq_gov_xperience;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_xperience)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
