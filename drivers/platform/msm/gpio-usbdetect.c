92a908548490ecb71996f92ab92de6f1338698b7
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2018-09-10 03:16:15 -0500

    complete our revert of this commit 2e3afa77de13ee8ceac7c712
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/platform/msm/gpio-usbdetect.c
+++ drivers/platform/msm/gpio-usbdetect.c
@@ -202,7 +202,7 @@ static void gpio_usbdetect_chg_work(struct work_struct *w)
 		dev_dbg(&usb->pdev->dev, "ID:%d VBUS:%d\n",
 						usb->id, usb->vbus);
 		if (!usb->id) {
-			gpio_usbdetect_notify_usb_type(usb,
+			power_supply_set_supply_type(usb->usb_psy,
 					POWER_SUPPLY_TYPE_UNKNOWN);
 			power_supply_set_present(usb->usb_psy, 0);
 			power_supply_set_usb_otg(usb->usb_psy, 1);
@@ -217,11 +217,11 @@ static void gpio_usbdetect_chg_work(struct work_struct *w)
 			gpio_set_value(usb->dpdm_switch_gpio, 0);
 
 		if (usb->vbus) {
-			gpio_usbdetect_notify_usb_type(usb,
+			power_supply_set_supply_type(usb->usb_psy,
 						POWER_SUPPLY_TYPE_USB);
 			power_supply_set_present(usb->usb_psy, usb->vbus);
 		} else {
-			gpio_usbdetect_notify_usb_type(usb,
+			power_supply_set_supply_type(usb->usb_psy,
 					POWER_SUPPLY_TYPE_UNKNOWN);
 			power_supply_set_present(usb->usb_psy, usb->vbus);
 		}
