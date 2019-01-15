92061411617feebbcb50067db1f80f0c7cf2bc1e
Francisco Franco <franciscofranco.1990@gmail.com>
2019-01-15 21:24:17 +0000

    cpufreq: one way or another I ain't letting perfd resetting the max freq after a user underclocks
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -614,6 +614,11 @@ static ssize_t store_##file_name					\
 	if (ret)							\
 		return -EINVAL;						\
 									\
+									\
+	if (&policy->object == &policy->max &&				\
+			!strncmp(current->comm, "perfd", 5))		\
+		return count;						\
+									\
 	new_policy.min = new_policy.user_policy.min;			\
 	new_policy.max = new_policy.user_policy.max;			\
 									\
